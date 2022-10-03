0d1e9c79f0cf07867d972bea21ea1b541b92449f
Sergio Pereira <72481029+sergio1pj@users.noreply.github.com>
2022-10-03 14:17:57 -0300

    feat: parte II OK

--- content/views.py
+++ content/views.py
@@ -1,3 +1,4 @@
+import sys
 from rest_framework.views import APIView, Request, Response, status
 from django.forms.models import model_to_dict
 from .models import Content
@@ -12,8 +13,36 @@ class ContentView(APIView):
         return Response(content_list, status=status.HTTP_200_OK)
        
     def post(self, request: Request) -> Response:
-        for key, value in request.data.items():
-            if not value:
-                return Response({'error': f'{key} is missing'}, status=status.HTTP_400_BAD_REQUEST)
-        content = Content.objects.create(**request.data)
-        return Response(model_to_dict(content), status=status.HTTP_201_CREATED)
+        set_keys = set(request.data.keys())
+        model_dict = {'title': str, 'module': str, 'description': str, 'students': int, 'is_active': bool}
+        set_fields = set(model_dict.keys())
+        diff = set_fields - set_keys
+        dict = {}
+        if len(diff) > 0:
+            for i in diff:
+                dict[i] = "missing key"
+            return Response(dict, status=status.HTTP_400_BAD_REQUEST)
+        else:
+            for key in set_fields:
+                if type(request.data[key]) != model_dict[key]:
+                    dict[key] = f'Must be {model_dict[key].__name__}'
+            if len(dict) > 0:
+                return Response(dict, status=status.HTTP_400_BAD_REQUEST)
+            else:
+                content = Content.objects.create(**request.data)
+                return Response(model_to_dict(content), status=status.HTTP_201_CREATED)
+
+
+    
+
+               
+       
+
+                 
+                
+
+           
+
+        
+      
+
