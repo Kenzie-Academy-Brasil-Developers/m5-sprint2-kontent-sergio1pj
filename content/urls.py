d784466d3aa55265939b2b611949f8488574057b
Sergio Pereira <72481029+sergio1pj@users.noreply.github.com>
2022-10-03 11:11:57 -0300

    fix: instalar rest_framework

--- content/urls.py
+++ content/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import ContentView
 
 urlpatterns = [
-    path('/contents', ContentView.as_view()),
+    path('contents', ContentView.as_view()),
 ]
--- content/views.py
+++ content/views.py
@@ -6,12 +6,14 @@ from .models import Content
 class ContentView(APIView):
     def get(self, request: Request) -> Response:
         contents = Content.objects.all()
-        content_list = [model_to_dict(content) for content in contents]
+        content_list = []
+        for content in contents:
+            content_list.append(model_to_dict(content))
         return Response(content_list, status=status.HTTP_200_OK)
        
     def post(self, request: Request) -> Response:
         for key, value in request.data.items():
             if not value:
-                return Response({'error': f'{key} is required'}, status=status.HTTP_400_BAD_REQUEST)
+                return Response({'error': f'{key} is missing'}, status=status.HTTP_400_BAD_REQUEST)
         content = Content.objects.create(**request.data)
         return Response(model_to_dict(content), status=status.HTTP_201_CREATED)
